eb8b621369abb171a18fc2099bf99ce1ebb4edd6
JoseAngelDev2 <casuhud1@gmail.com>
2026-02-07 15:30:59 -0400

    Message public

---
+++ Music/Proyecto borrador V2 c#/Task 1/Entities/Maestro.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CURSO_C_.Entities
+{
+    public class Maestro : Docente
+    {
+        string NivelEducativo {get; set;}
+    }
+}
