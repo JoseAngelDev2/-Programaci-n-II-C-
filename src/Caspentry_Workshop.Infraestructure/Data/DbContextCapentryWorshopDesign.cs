ba7919fcf8717e4f4f705acd59f856e1e05cc2ea
JoseAngelDev2 <casuhud1@gmail.com>
2026-04-25 17:34:32 -0400

    Update database connection string to use placeholder password
    Replaced the database password with a placeholder for security.

--- src/Caspentry_Workshop.Infraestructure/Data/DbContextCapentryWorshopDesign.cs
+++ src/Caspentry_Workshop.Infraestructure/Data/DbContextCapentryWorshopDesign.cs
@@ -12,7 +12,7 @@ namespace Caspentry_Workshop.Infraestructure.Data
           public AppCarpentry CreateDbContext(string[] args)
         {
             var optionsBuilder = new DbContextOptionsBuilder<AppCarpentry>();
-            optionsBuilder.UseNpgsql("Host=aws-1-us-west-2.pooler.supabase.com;Database=postgres;Username=postgres.obcwgoqpwpxgxohlzdhs;Password=Uncle.com1424;SSL Mode=Require;Trust Server Certificate=true");
+            optionsBuilder.UseNpgsql("Host=aws-1-us-west-2.pooler.supabase.com;Database=postgres;Username=postgres.obcwgoqpwpxgxohlzdhs;Password=*;SSL Mode=Require;Trust Server Certificate=true");
             return new AppCarpentry(optionsBuilder.Options);
         }
     }
